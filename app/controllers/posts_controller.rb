18ac7ad3bb622c58fbc4e01b8b42ff8bd75d6f38
nakany <flashkmfdm@gmail.com>
2017-05-04 21:28:46 +0900

    index and controller modified

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
   	@posts = Post.all
   end
+  def create
+    @post = Post.new(content:params[:content])
+  	@post.save
+
+  	redirect_to("/posts/index")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'posts/index' =>"posts#index"
 
   root :to => 'home#top'
+  post "posts/index" => "posts#create"
   get 'home/top' 
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
