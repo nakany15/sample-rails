8bb97f8401a5cd5a18140b7d192b3b283d1b66d0
nakany <flashkmfdm@gmail.com>
2017-05-05 19:05:39 +0900

    added edit-delete features

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root :to => 'home#top'
   get 'posts/index' =>"posts#index"
   post "posts/index" => "posts#create"
   
   get 'posts/:id' => 'posts#show'
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
-  root :to => 'home#top'
+  post "posts/:id/destroy" => "posts#destroy"
   get 'home/top' 
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
