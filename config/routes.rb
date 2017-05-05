b2c1afa97a921e7c48b2b4271223a5ba8f1fd06d
nakany <flashkmfdm@gmail.com>
2017-05-05 18:00:43 +0900

    added some features

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'posts/index' =>"posts#index"
-
-  root :to => 'home#top'
   post "posts/index" => "posts#create"
+  
+  get 'posts/:id' => 'posts#show'
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  root :to => 'home#top'
   get 'home/top' 
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
